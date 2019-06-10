1108d019b31fce015d5e4a44ada6a7af3dfeb69a
admin <mas@mysteel.com>
2019-06-10 11:50:52 +0800

    代码更新

--- NetCoreApi.ServiceStack.ServiceModel/Request/HealthCheckRequest.cs
+++ NetCoreApi.ServiceStack.ServiceModel/Request/HealthCheckRequest.cs
@@ -1,10 +1,13 @@
 ﻿using NetCoreApi.ServiceStack.ServiceModel.Response;
 using ServiceStack;
+using ServiceStack.DataAnnotations;
 
 namespace NetCoreApi.ServiceStack.ServiceModel.Request
 {
     [Api("Consul健康检查")]
     [Route("/api/health", Verbs = "GET", Summary = "health", Notes = "health")]
+    // 隐藏接口
+    [Exclude(Feature.All)]
     public class HealthCheckRequest : IReturn<ApiResponse<string>>
     {
     }
--- NetCoreApi.ServiceStack.ServiceModel/Request/HelloWorldRequest.cs
+++ NetCoreApi.ServiceStack.ServiceModel/Request/HelloWorldRequest.cs
@@ -4,6 +4,11 @@ using ServiceStack.DataAnnotations;
 
 namespace NetCoreApi.ServiceStack.ServiceModel.Request
 {
+    /**
+     * 标记接口的请求方式
+     * 1、 public class HelloWorldRequest : IGet, IReturn<>
+     * 2、 [Route("", Verbs = "GET", Summary = "")]
+     */
     [Api("ServiceStack service类请求")]
     [Route("/service/first", Verbs = "GET", Summary = "helloworld", Notes = "Notes")]
     public class HelloWorldRequest : IReturn<ApiResponse<string>>
